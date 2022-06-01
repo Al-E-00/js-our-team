cd76d6da89979519961ed31daff58613b59d235a
Al-E-00 <mazzellaalessio6@gmail.com>
2022-06-01 13:52:55 +0200

    Our team project ->setted up

---
+++ js/app.js
@@ -0,0 +1,31 @@
+/* 
+    For "How we are" section of our business website, we have to 
+    develop "our team" section.
+    We must establish an array of objects who represents the team members.
+    Every member has it's own personal information necessary to print
+    out the relative card: Name, rule, photo.
+*/
+/* 
+MILESTONE 1: 
+print on console members list, writing separetely every component details
+*/
+/* 
+MILESTONE 2: 
+print out datas inside a container into html page dynamically, 
+creating for each member of the team a html element that contains their data.
+*/
+/* 
+BONUS:
+Stylize section creating each member of the team card
+*/
+/*
+TIPS:
+1. We think in steps
+2. Write logic in English, then translate into code
+3. Remember console.log() and console.table()
+4. If a piece of code works, let's ask ourself if we can divide it in 
+smaller funcitons
+5. What contain the fied that represent objects picture?
+6. How we can tranform it into a visible image inside the html?
+*/
+
